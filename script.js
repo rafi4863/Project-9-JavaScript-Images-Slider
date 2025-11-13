9348362b3e9084e29e84b52c8e840fd37caa1781
rafi4863 <rafi2626r@gmail.com>
2025-11-13 14:47:16 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let currentIndex = 0;
+
+const showSlide = (index) => {
+   const slides = document.getElementById('slides');
+   const totalSlides = slides.children.length;
+
+   if (index >= totalSlides) {
+        currentIndex = 0;
+   }
+   else if (index < 0) {
+     currentIndex = totalSlides - 1;
+   }
+   else{
+    currentIndex = index;
+   }
+
+   slides.style.transform = `translateX(${-currentIndex * 100}%)`;
+}
+
+
+const nextSlide = () => {
+    showSlide(currentIndex + 1);
+}
+
+const prevSlide = () => {
+    showSlide(currentIndex - 1);
+}
